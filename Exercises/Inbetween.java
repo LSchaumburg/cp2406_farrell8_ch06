de3ea6422e56e436bf9e8b0b12d9790f23620b3f
jc247746 <luke.schaumburg@my.jcu.edu.au>
2016-08-22 11:19:12 +1000

    Completed Extra Exercises

--- Exercises/Inbetween.java
+++ Exercises/Inbetween.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by jc247746 on 22/08/16.
  */
 public class Inbetween {
     public static void main(String[] args) {
+        int firstInput;
+        int secondInput;
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter low number: ");
+        firstInput = scanner.nextInt();
+        System.out.println("Enter high number: ");
+        secondInput = scanner.nextInt();
+
+        while (firstInput > secondInput) {
+            System.out.println("Low number is higher than high number");
+            System.out.println("Enter low number: ");
+            firstInput = scanner.nextInt();
+            System.out.println("Enter high number: ");
+            secondInput = scanner.nextInt();
+        }
+
+        number = firstInput;
+        while (number <= secondInput) {
+            System.out.println(number + " ");
+            number += 1;
+        }
 
     }
 }
